992ef67b1e4f916b1d49e66a0589016a50b5e9af
shashiweb <shashiweb64@gmail.com>
2024-08-20 17:29:08 +0530

    change

--- reactclient/src/modules/auth/Myloginpage.js
+++ reactclient/src/modules/auth/Myloginpage.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React  , {useState} from 'react'
 import { HiOutlineMail } from "react-icons/hi";
 import { TbPasswordFingerprint } from "react-icons/tb";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
 
 function Myloginpage() {
+    const usenav = useNavigate();
+
+
+    const [userlogin, usersetlogin] = useState({
+        email: "",
+        pass: ""
+    });
+
+
+    const updateinput = (e) => {
+        const { name, value } = e.target;
+        // console.log(e);
+        usersetlogin((a) => {
+            return {
+                ...a,
+                [name]: value
+            }
+        })
+    }
+
+
+    const mylogin = async()=>{
+
+        const { email, pass } = userlogin;  
+        if(email==="" || pass==="")
+        {
+            alert("email and  password is blank");
+        }
+        else
+        {
+
+        const mydata = await fetch("http://localhost:5782/mylogin", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email,pass
+            })
+        });
+        const res = await mydata.json();
+            console.log(res);
+            if(res.status===422)
+            {
+                alert("welcome to login page");
+                usenav('/dashboard');
+
+            }
+            else
+            {
+                alert("can't login page");
+            }
+    }
+    }
+
+
+
+
+
+
+
   return (
     <div className='container'>
             <div className='row justify-content-md-center'>
@@ -12,14 +72,14 @@ function Myloginpage() {
                         <div className='row'>
                             <div className='col-12 p-2 mt-2'>
                                 <label className="form-label"> <HiOutlineMail /> Email address</label>
-                                <input type='text' className='form-control' placeholder='email id'/>
+                                <input type='text' className='form-control' placeholder='email id' name='email' onChange={updateinput}/>
                             </div>
                             <div className='col-12 p-2 mt-2'>
                                 <label className="form-label"> <TbPasswordFingerprint /> Password</label>
-                                <input type='password' className='form-control' placeholder='password'/>
+                                <input type='password' className='form-control' placeholder='password' name='pass' onChange={updateinput}/>
                             </div>
                             <div className='col-12 p-2 mt-2 text-center'>
-                                <input type='button' value="submit" className='btn btn-success'/>
+                                <input type='button' value="submit" className='btn btn-success' onClick={mylogin}/>
                                 <input type='reset' value="cancel" className='btn btn-danger ms-3' />
                                 <Link to="registor">New Registor</Link>
                                 <Link to="dashboard" className='btn'>dashboard</Link>
