8b36fe1cb30b251fa1cbd397c9c368ddea455bfa
shashiweb <shashiweb64@gmail.com>
2024-09-04 17:37:34 +0530

    finished class

--- reactclient/src/shares/Mynavbar.js
+++ reactclient/src/shares/Mynavbar.js
@@ -1,50 +1,75 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom';
 import { FaWhatsapp } from "react-icons/fa";
 
 function Mynavbar() {
   return (
+    <Fragment>
     <nav className="navbar navbar-expand-lg bg-body-tertiary shadow">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Application
-            </a>
-            <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="#">Dashboard</a></li>
-              <li><a className="dropdown-item" href="#">modules1</a></li>
-              <li><a className="dropdown-item" href="#">modules2</a></li>
-              <li><a className="dropdown-item" href="#">modules3</a></li>
-              <li><a className="dropdown-item" href="#">modules4</a></li>
-              <li><a className="dropdown-item" href="#">modules5</a></li>
-            </ul>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link h1" to="https://api.whatsapp.com/send/?phone=+91-9871672942&text=Hi%21&type=phone_number&app_absent=0"><FaWhatsapp/></Link>
-          </li>
-        </ul>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Application
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Dashboard</a></li>
+                <li><a className="dropdown-item" href="#">modules1</a></li>
+                <li><a className="dropdown-item" href="#">modules2</a></li>
+                <li><a className="dropdown-item" href="#">modules3</a></li>
+                <li><a className="dropdown-item" href="#">modules4</a></li>
+                <li><a className="dropdown-item" href="#">modules5</a></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link h1" to="https://api.whatsapp.com/send/?phone=+91-9871672942&text=Hi%21&type=phone_number&app_absent=0"><FaWhatsapp /></Link>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#login">
+              login
+            </button>
+          </form>
+        </div>
       </div>
+    </nav>
+
+<div class="modal custom-model fade" id="login" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
-  </nav>
+    <div class="modal-body">
+      ...
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      <button type="button" class="btn btn-primary">Save changes</button>
+    </div>
+  </div>
+</div>
+</div>
+</Fragment>
+
+
   )
 }
 
